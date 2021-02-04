9ecc85efde6bd32c6e872d69458763158a814c06
Venkat Jayanarasimhan <72488705+Venkat-J@users.noreply.github.com>
2021-02-04 22:59:20 +0530

    Add files via upload

---
+++ tokens.R
@@ -0,0 +1,7 @@
+#Twitter App Details
+
+appname <- "SocialMediaVenkat"
+consumer_key <- "xiF73e4uwcblfjB1TatRV7mNB"
+consumer_secret <- "mdQrx2EHlVyxLS7B5kfBSjl9vDXdfWc6DUkGEVe5ZeQHQDXNZR"
+access_token <- "1350465971111092224-xbWg46f1RBArPgwivTEIACNmlQ1R4W"
+access_secret <- "xlJaN8tUA7yLu0JEeYoddiZpmvSTANMc2pe75YWrayh4A"
